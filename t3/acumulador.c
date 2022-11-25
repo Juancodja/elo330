e37588efe8ce3a72a01d6e6833036e0d18787703
Juan M. Codjambassis <juancodja@gmail.com>
2022-11-25 10:49:40 -0300

    oli

---
+++ t3/acumulador.c
@@ -0,0 +1,79 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+
+
+#define PORTNUMBER  47200
+
+void* handler(void* args);
+
+int main(int argc, char* argv[])
+{
+    char buf[10];
+    int s, n , ns;
+    unsigned int portnumber;
+    socklen_t addr_len;
+    struct sockaddr_in name;
+    
+    pthread_t handler_thread;
+    pthread_create(&handler_thread, NULL, handler, NULL);
+    pthread_join(handler_thread, NULL);
+
+    switch (argc)
+    {
+    case 1:
+        portnumber = PORTNUMBER;
+        break;
+    case 2:
+        portnumber = atoi(argv[1]);
+        break;
+    default:
+        break;
+    }
+
+
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+
+    name.sin_family = AF_INET;
+    name.sin_port = htons(portnumber);
+    name.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr_len = sizeof(struct sockaddr_in);
+
+    if( bind(s, (struct sockaddr *) &name, addr_len))
+		printf("bind error");
+
+    listen(s, 5);
+
+    for( ; ; ){
+        fflush(stdout);
+        ns = accept(s, (struct sockaddr *)&name, &addr_len);
+        printf("Connection from : %s\n",inet_ntoa(name.sin_addr));
+
+            while ((n = recv(ns, buf, sizeof(buf), 0)) > 0)
+            write(1, buf, n);
+    }
+    close(ns);
+    close(s);
+    exit(0);
+    
+}
+
+
+
+int pthread_create(pthread_t *restrict thread, const pthread_attr_t *restrict attr,
+                          void *(*start_routine)(void *),
+                          void *restrict arg);
+
+
+void* handler(void* arg){
+    printf("Soy un PeHilo\n");
+
+    return (void *) NULL;
+}
---
+++ t3/recuento.c
@@ -0,0 +1,80 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/*
+int send_vote(int __fd, const void* __buf, ssize_t __n, int __flags){
+    char votoA[1024] = "a", votoB[1024] = "b";
+    if(strcmp(__buf, votoA) == 0){
+        send(__fd, __buf, __n, __flags);
+        return 0;
+    }
+    else if(strcmp(__buf, votoB) == 0){
+        send(__fd, __buf, __n, __flags);
+        return 0;
+    }
+    else if(strcmp(__buf, "f") == 0){
+        printf("Finalizando Votación.\n");
+        return -1;
+    }
+    else{
+        printf("Voto incorrecto opciones {a, b}\n");
+        return 0;
+    }
+}
+*/
+
+
+int main(int argc, char * argv[])
+{
+    int n, s, len; 
+    char buf[1024];
+    char hostname[64];
+    unsigned int portnumber = 47200;
+    struct hostent *hp;
+    struct sockaddr_in name;
+
+    switch (argc)
+    {
+    case 1:
+        gethostname(hostname, sizeof(hostname));
+        break;
+    case 2:
+        strcpy(hostname, argv[1]);
+        break;
+    case 3:
+        strcpy(hostname, argv[1]);
+        portnumber = atoi(argv[2]);
+        break;
+    default:
+        break;
+    }
+    
+    hp = gethostbyname(hostname);
+
+    s = socket(AF_INET, SOCK_STREAM, 0);
+
+    name.sin_family = AF_INET;
+    name.sin_port = htons(portnumber);
+
+    memcpy(&name.sin_addr, hp->h_addr_list[0], hp->h_length);
+    len = sizeof(struct sockaddr_in);
+
+    connect(s, (struct sockaddr *) &name, len);
+
+    while ((n = read(0, buf, sizeof(buf))) > 0) {
+        if (send(s, buf, n, 0) < 0) {
+            perror("send");
+            exit(1);
+        }
+    }
+
+    close(s);
+    exit(0);
+}
