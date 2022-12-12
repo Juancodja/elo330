9973d141cfa7a8febd4763331d375ade2c220fb7
Juan M. Codjambassis <juancodja@gmail.com>
2022-12-12 19:58:06 -0300

    balas

--- proyecto/httpserver/script.js
+++ proyecto/httpserver/script.js
@@ -1,13 +1,16 @@
 const socket = new WebSocket('ws://localhost:47201');
 
-const circle = document.querySelector('.circle');
-let moveBy = 10;
-let playerName = "player"
-window.addEventListener('load', ()=>{
-    circle.style.position = 'absolute';
-    circle.style.left = 0;
-    circle.style.top = 0; 
+const PLAYERSIZE  = 40;
+const BULLETSIZE  = 15; 
 
+let canvas = document.getElementById("mainCanvas");
+var context = canvas.getContext("2d");
+
+let playerName = prompt("Ingrese player Name")
+
+
+
+window.addEventListener('load', ()=>{
 })
 
 socket.addEventListener("open", e => {
@@ -17,16 +20,15 @@ socket.addEventListener("open", e => {
 
 socket.onmessage = ({ data }) => {
     console.log('Message from server', data);
-    let myArr = JSON.parse(data);
-    let players = myArr.Players
+    let gameData = JSON.parse(data);
 
-    for ( let i in players){
-        circle.style.left = parseInt(players[i].posX) +'px';
-        circle.style.top = parseInt(players[i].posY) +'px';
-        console.log(circle.style.top, circle.style.left);
-    }
+    draw(gameData);
 
 };
+
+var intervalId = setInterval(function() {
+    socket.send("{}")
+  }, 10);
 
 window.addEventListener('keydown', move);
 
@@ -35,7 +37,7 @@ window.addEventListener('click', (e)=>{
         "name": playerName,
         "mouseX": 0,
         "mouseY": 0
-    }
+        }
     }
     shoot.shoot.mouseX = e.clientX;
     shoot.shoot.mouseY = e.clientY;
@@ -67,4 +69,54 @@ function move(e){
     }
     message.move.dir = dir;
     socket.send(JSON.stringify(message));
+}
+
+
+function draw(gameData){
+    let players = gameData["Players"];
+    let bullets = gameData["bullets"];
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    for(let i in players){  
+        let R = players[i].R;
+        let G = players[i].G; 
+        let B = players[i].B;
+
+        let color = '#' + R.toString(16) + G.toString(16) + B.toString(16);
+        drawPoint(context, players[i].posX, players[i].posY, players[i].name, color, PLAYERSIZE);
+
+    }
+    for(let i in bullets){
+        drawPoint(context, bullets[i].posX, bullets[i].posY, null, null, BULLETSIZE);
+    }
+}
+
+
+function drawPoint(context, x, y, label, color, size) {
+    if (color == null) {
+        color = '#000'; 
+    }
+    if (size == null) {
+        size = 5;
+    }
+
+    var radius = 0.5 * size;
+
+    // to increase smoothing for numbers with decimal part
+    var pointX = Math.round(x - radius);
+    var pointY = Math.round(y - radius);
+
+    context.beginPath();
+    context.fillStyle = color;
+    context.fillRect(pointX, pointY, size, size);
+    context.fill();
+
+    if (label) {
+        var textX = Math.round(x);
+        var textY = Math.round(pointY - 5);
+
+        context.font = 'Italic 14px Arial';
+        context.fillStyle = color;
+        context.textAlign = 'center';
+        context.fillText(label, textX, textY);
+    }
 }
